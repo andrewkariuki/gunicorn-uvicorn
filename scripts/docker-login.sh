55b99fc593811b661f96e09fc69599d12bf18017
Andrew Kariuki <andrew.kariuki@wiocc.net>
2023-03-08 22:43:15 +0300

    chore: improve on structure

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-if [ "$(echo "$CONTAINER_REGISTRY_PASSWORD" | docker login ghcr.io -u "$CONTAINER_REGISTRY_USERNAME" --password-stdin 2>&1)" -eq 0 ]; then
+LOGIN_ERRORS=$(echo "$CONTAINER_REGISTRY_PASSWORD" | docker login ghcr.io -u "$CONTAINER_REGISTRY_USERNAME" --password-stdin 2>&1)
+if [ $? -eq 0 ]; then
+    echo "$LOGIN_ERRORS"
     echo "Successfully logged in to container registry"
     exit 0
+else
+    echo "Failed to login container registry"
+    echo "$LOGIN_ERRORS"
+    exit 1
 fi
